4645bd004340b8b11930e68e671622034f79e2ab
repserc-ico <kameshima@std.it-college.ac.jp>
2026-05-26 04:47:13 +0000

    家計簿SQLスクリプト

---
+++ kakeibo.sql
@@ -0,0 +1,20 @@
+--データベース作成(既に同名のデータベースがあれば削除する)
+CREATE OR REPLACE DATABASE kakeibo;
+--データベースの切替
+USE kakeibo;
+--家計簿テーブルの作成とデータのセット
+CREATE TABLE 家計簿 (
+	日付 DATE,
+	費目 VARCHAR(20),
+	メモ VARCHAR(100),
+	入金額 INTEGER(10),
+	出金額 INTEGER(10)
+);
+INSERT INTO 家計簿 VALUES('2024-02-03','食費','コーヒーを購入',0,380);
+INSERT INTO 家計簿 VALUES('2024-02-10','給料','1月の給料',280000,0);
+INSERT INTO 家計簿 VALUES('2024-02-11','教養娯楽費','書籍を購入',0,2800);
+INSERT INTO 家計簿 VALUES('2024-02-14','交際費','同期会の会費',0,5000);
+INSERT INTO 家計簿 VALUES('2024-02-18','水道光熱費','1月の電気代',0,7560);
+SHOW TABLES;
+DESCRIBE 家計簿
+SELECT * FROM 家計簿
